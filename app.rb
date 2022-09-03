63149baa7a6bcfe01c889162852c8594811d2ed8
jean p saldana <jp.saldana@outlook.es>
2022-09-03 15:19:39 -0500

    avance

--- app.rb
+++ app.rb
@@ -3,4 +3,46 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
+configure :development do
+  use Rack::MethodOverride
+end
 
+# acceso al Root
+get "/" do
+
+end
+
+# redireccionando a la pagina para mostrar los Restaurantes
+get "/restaurants" do
+
+end
+
+# redireccionamos para crear nuevos restaurantes
+get "/restaurants/new" do
+
+end
+
+# guardamos los datos creados por el NEW
+post "/restaurants" do
+
+end
+
+# buscar el id para modificar
+get "/restaurants/:id" do
+
+end
+
+# para editar datos
+get "/restaurants/:id/edit" do
+
+end
+
+# para guardar los datos editados en el GET
+patch "/restaurants/:id" do
+
+end
+
+# elimina el ID que vayamos a modificar
+delete "/restaurants/:id" do
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
+puts "creanting restaurants"
 
+Restaurant.create(
+  name: "batch 905",
+  city: "Lima",
+  image_url: "https://imgmedia.buenazo.pe/650x358/buenazo/original/2022/05/17/6284236d6b33a70c9d423d0c.jpg",
+  phone_nomber: "87239342342",
+  address: "Av Lima 984",
+  type_food: "Comida peruana"
+)
+
+Restaurant.create(
+  name: "La barca de Luis Mi",
+  city: "Mexico",
+  image_url: "https://t2.rg.ltmcdn.com/es/posts/7/4/1/ceviche_peruano_18147_600.jpg",
+  phone_nomber: "45645434",
+  address: "Av Acapulco",
+  type_food: "Comida marina peruana"
+)
